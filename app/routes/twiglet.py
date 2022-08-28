0ac470f86b389a5cf57ed4ca7133bb7616daa731
Florencia Pezcara <fpezcara@gmail.com>
2022-08-28 14:05:09 +0100

    hashed password

--- app/routes/twiglet.py
+++ app/routes/twiglet.py
@@ -40,10 +40,9 @@ def get_all_twiglets():
      
 
         existing_location = Twiglet.query.filter_by(latitude=latitude, longitude=longitude).first()
-        print(existing_location)
         if existing_location:
             existing_location.date_last_confirmed = datetime.datetime.utcnow()
-            return jsonify("Tiglet was update!"), 201
+            return jsonify("Tiglet was updated!"), 201
         new_twiglet = Twiglet(longitude=longitude, latitude=latitude, shop_name=shop_name, address=address, found_by_user=found_by_user, date_found=date_found, date_last_confirmed=date_last_confirmed)       
 
         db.session.add(new_twiglet)

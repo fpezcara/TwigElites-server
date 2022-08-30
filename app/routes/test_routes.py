7d702c9c601a71b9d0e071475cd3c2a4a5313b9a
saamiya96 <saamiyayousuf@hotmail.co.uk>
2022-08-30 14:16:24 +0100

    added testings!

--- app/routes/test_routes.py
+++ app/routes/test_routes.py
@@ -5,3 +5,39 @@ def test_welcome(api):
     app = api.get('/')
     assert app.status == '200 OK'
     assert app.get_data() ==  b"<h1>Twigelites Server!</h1>"
+  
+def test_register(api):
+    user_data = json.dumps({'username': "tester", 'email': 'flo@gmail.com', 'password': 'test'})
+    mock_headers = {'Content-Type': 'application/json'}
+    app = api.post('/auth/register', data=user_data, headers=mock_headers)
+
+# def_test_register_post(api):
+
+def test_login(api):
+    user_data = json.dumps({'username': "tester",'password': 'test'})
+    mock_headers = {'Content-Type': 'application/json'}
+    app = api.post('/auth/login', data=user_data, headers=mock_headers)
+
+def test_401(api):
+    app = api.post('/auth/login')
+    user_data = json.dumps({'username': "tester", 'password': ''})
+    mock_headers = {'Content-Type': 'application/json'}
+    app = api.post('/auth/login', data=user_data, headers=mock_headers)
+
+def test_get_all_twiglets(api):
+    app = api.get('/twiglets')
+    assert app.status == '200 OK'
+    assert b'twiglet_id' in app.get_data()
+
+# def test_get_twigletid(api):
+#     app = api.get('/twiglets/1')
+#     assert app.status == '200 0K'
+#     assert b'Some shop' in app.get_data()
+
+def test_get_all_users(api):
+    app = api.get('/auth/users')
+    assert app.status == '200 OK'
+    assert b'email' in app.get_data()
+
+# def test_delete_twiglet(api):
+#     app = api.delete('/twiglets')

7f4a6b39ed3e455a731ec239cd61ef59af301d02
Florencia Pezcara <fpezcara@gmail.com>
2022-08-31 21:31:32 +0100

    added socket io

---
+++ app/routes/socket_io.py
@@ -0,0 +1,28 @@
+
+from flask_socketio import SocketIO, emit, send, join_room
+from flask import request
+
+def start_socket(socketio):
+
+    @socketio.on('connect')
+    def connect():
+        join_room(request.values['id'])
+        print((request.values['id']))
+        print("****CONNECTED!*****")
+
+    @socketio.on('disconnect')
+    def disconnect():
+        print("****DISCONNECTED!*****")
+
+    @socketio.on('send-message')
+    def get_message(data):
+        recipients = data['recipients']
+        text = data['text']
+
+        for recipient in recipients:
+            new_recipients = [r for r in recipients if r!= recipient]
+            new_recipients.append(request.values['id'])
+            json_data = {
+                    "recipients": new_recipients, "sender": request.values['id'], "text":text
+                }
+            emit("receive-message", json_data, room=recipient)
